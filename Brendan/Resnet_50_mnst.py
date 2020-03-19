7578c0e42d5e71f9caf5372183102974d2437bdd
Hand-B <bdhand@live.com>
2020-03-19 15:53:54 -0400

    Updated The Code to Reflect the Areas Im Adding Next
    Included notes about pytorch implementation barriers.

--- Brendan/Resnet_50_mnst.py
+++ Brendan/Resnet_50_mnst.py
@@ -4,10 +4,10 @@ Created on Mon Mar 16 23:57:04 2020
 
 @author: bdhan
 """
-
-
+from PIL import Image
 import torch
 import torchvision
+from torchvision import transforms
 import numpy as np
 
 ### CRedit for synthetic mnist data set from: 
@@ -20,14 +20,27 @@ test_y = np.load("./data/test_x.npy")
 
 ## This guide was used as a baisis for preparing the transfer learning:
 ## https://missinglink.ai/guides/pytorch/pytorch-resnet-building-training-scaling-residual-networks-pytorch/
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
 
-input_tensor = preprocess(tainImages_x[0])
+## TODO add Dataset either implemnation or via tensors torch.utils.data.Dataset 
+for img in tainImages_x:
+    tempImage = Image.fromarray(np.uint8(img*255))
+    input_tensor = preprocess(tempImage)
 input_batch = input_tensor.unsqueeze(0)
+
+#inport model
 model = torch.hub.load('pytorch/vision:v0.5.0', 'resnet50', pretrained=True)
-#num_model_features = model.fc.in_features
-#model.fc = torch.nn.Linear(num_model_features,9)
-#
-#model.train(tainImages_x,tainImages_y))
+#update length of output
+num_model_features = model.fc.in_features
+model.fc = torch.nn.Linear(num_model_features,9)
+
+# train via transfer learning
+
 with torch.no_grad():
     output = model(input_batch)
 print(output[0])
