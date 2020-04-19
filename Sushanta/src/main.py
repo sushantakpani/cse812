e37a18d3c83d08667d8878a7a930588a323e4c5e
Sushanta K. Pani <panisush@msu.edu>
2020-04-19 17:59:44 -0400

    Update main.py

--- Sushanta/src/main.py
+++ Sushanta/src/main.py
@@ -7,6 +7,10 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 import torch.multiprocessing as mp
+import os
+
+
+os.environ["CUDA_VISIBLE_DEVICES"]="1,3"
 
 
 class Net(nn.Module):
@@ -30,21 +34,21 @@ class Net(nn.Module):
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=128, metavar='N',
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=10, metavar='N',
                         help='number of epochs to train (default: 10)')
     parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-                        help='learning rate (default: 0.01)')
+                        help='learning rate (default: 0.001)')
     parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
                         help='SGD momentum (default: 0.5)')
-    parser.add_argument('--no-cuda', action='store_true', default=True, ## Change from False to True
+    parser.add_argument('--no-cuda', action='store_true', default=False, ## Change from False to True
                         help='disables CUDA training')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+    parser.add_argument('--log-interval', type=int, default=400, metavar='N',
                         help='how many batches to wait before logging training status')
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
@@ -52,14 +56,19 @@ def main():
                         help='how many training processes to use (default: 2)')
 
     args = parser.parse_args()
+    
     use_cuda = not args.no_cuda and torch.cuda.is_available()
-
     torch.manual_seed(args.seed)
-
-    device = torch.device("cuda" if use_cuda else "cpu")
-
+    device = torch.device("cuda:0" if use_cuda else "cpu")
+    print("Device",device)
+    #cuda_list=torch.cuda.device_count()
+    #print("list", cuda_list)
+    #device = torch.device("cuda")
+    #print(device, "is", torch.cuda.current_device() )
 
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    
+    
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST('../TempData/data', train=True, download=True,
                        transform=transforms.Compose([
@@ -67,6 +76,8 @@ def main():
                            transforms.Normalize((0.1307,), (0.3081,))
                        ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
+    
+    
     test_loader = torch.utils.data.DataLoader(
         datasets.MNIST('../TempData/data', train=False, transform=transforms.Compose([
             transforms.ToTensor(),
@@ -74,27 +85,41 @@ def main():
         ])),
         batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
+    
+    #model = Net().to(device)
+    #model=Net().cuda()
+    
+    
+    model = Net()
+    print("batch_size", args.batch_size, "learning rate ", args.lr)
+    if (torch.cuda.device_count() > 0) and (device!=torch.device("cpu")):
+        print("Let's use", torch.cuda.device_count(), "GPUs!")
+        # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
+        model = nn.DataParallel(model)
 
-    model = Net().to(device)
+    model.to(device)
+    
     #model = torch.nn.DataParallel(model, device_ids=None)
-    model.share_memory()  #Multi-processing step
+    
+    #model.share_memory()  #Multi-processing step
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
     t = time.time()
     for epoch in range(1, args.epochs + 1):
+        print(epoch," epoch of total", args.epochs)
       # For using multi-processing
-       processes = []
-       for rank in range(args.num_processes):
-         p = mp.Process(target=train, args=(args, model, device, train_loader, optimizer, epoch))
-         p.start()
-         processes.append(p)
-       if bool(epoch%2) == 'False':
-           for p in processes:
-             p.join()
+            #         processes = []
+            #         for rank in range(args.num_processes):
+            #             p = mp.Process(target=train, args=(args, model, device, train_loader, optimizer, epoch))
+            #             p.start()
+            #             processes.append(p)
+            #         if bool(epoch%2) == 'False':
+            #             for p in processes:
+            #                 p.join()
 
       # For not using multi-processing
-       #train(args, model, device, train_loader, optimizer, epoch)
-       test(args, model, device, test_loader)
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(args, model, device, test_loader)
 
     print('Total Time taken in training and validation: '+ str(time.time() - t))
 
@@ -106,13 +131,14 @@ def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
+        #data, target = data.cuda(), target.cuda()
         optimizer.zero_grad()
         output = model(data)
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            print('Train Epoch>>>>>>>: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                        100. * batch_idx / len(train_loader), loss.item()))
 
@@ -124,6 +150,7 @@ def test(args, model, device, test_loader):
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
+            #data, target = data.cuda(), target.cuda()
             output = model(data)
             test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
@@ -135,5 +162,6 @@ def test(args, model, device, test_loader):
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
+    
 if __name__ == '__main__':
     main()
